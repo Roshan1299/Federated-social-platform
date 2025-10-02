aac5a988fcb98261ea7eb84bbbec0814ea7ef178
Roshan1299 <banisettirosh@gmail.com>
2025-10-01 22:20:29 -0600

    feat: Implement user sign-up form, view, and template

---
+++ authors/forms.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import Author
+
+class AuthorCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Author
+        fields = UserCreationForm.Meta.fields + ('displayName', 'github',)
--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from .forms import AuthorCreationForm
+
+class SignUpView(CreateView):
+    form_class = AuthorCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.is_active = False
+        user.save()
+        return super().form_valid(form)
