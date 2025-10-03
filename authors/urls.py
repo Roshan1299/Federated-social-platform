aeca323f255d759c896117d3af2a0ab7bcfa0457
Roshan1299 <banisettirosh@gmail.com>
2025-10-03 00:20:14 -0600

    feat: Add author profile and API endpoints

--- authors/urls.py
+++ authors/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, AuthorProfileView, AuthorAPIView
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
+    path("accounts/signup/", SignUpView.as_view(), name="signup"),
+    path("authors/<uuid:author_id>/", AuthorProfileView.as_view(), name="author_profile"),
+    path("api/authors/<uuid:author_id>/", AuthorAPIView.as_view(), name="author_api"),
 ]
--- authors/views.py
+++ authors/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic.edit import CreateView
+from django.views.generic import DetailView
+from django.views import View
+from django.http import JsonResponse
+from .models import Author
 
 from .forms import AuthorCreationForm
 
@@ -12,5 +16,33 @@ class SignUpView(CreateView):
     def form_valid(self, form):
         user = form.save(commit=False)
         user.is_active = False
+
+        host = self.request.scheme + "://" + self.request.get_host()
+        user.host = host
         user.save()
-        return super().form_valid(form)
+
+        api_url = reverse('author_api', kwargs={'author_id': user.id})
+        user.url = host + api_url
+        user.save(update_fields=['url'])
+
+        return redirect(self.success_url)
+
+class AuthorProfileView(DetailView):
+    model = Author
+    template_name = "authors/profile.html"
+    pk_url_kwarg = "author_id"
+
+class AuthorAPIView(View):
+    def get(self, request, author_id):
+        author = Author.objects.get(id=author_id)
+        web_url = reverse('author_profile', kwargs={'author_id': author.id})
+        data = {
+            "type": "author",
+            "id": author.url, 
+            "host": author.host,
+            "displayName": author.displayName,
+            "github": author.github,
+            "profileImage": author.profileImage,
+            "web": request.scheme + "://" + request.get_host() + web_url,
+        }
+        return JsonResponse(data)
