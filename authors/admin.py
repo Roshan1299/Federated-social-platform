0d66b868b4e2c12891c0d8b3a870a2b664b12f15
Roshan1299 <banisettirosh@gmail.com>
2025-10-03 00:19:54 -0600

    feat: Add UUID identity and configure admin panel

--- authors/admin.py
+++ authors/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 from .models import Author
 
-# Register your models here.
-admin.site.register(Author)
+class AuthorAdmin(UserAdmin):
+    model = Author
+    list_display = ('username', 'displayName', 'email', 'is_staff', 'is_active')
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("displayName", "first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+admin.site.register(Author, AuthorAdmin)
--- authors/models.py
+++ authors/models.py
@@ -1,7 +1,11 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Author(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    host = models.URLField(blank=True, null=True)
+    url = models.URLField(blank=True, null=True)
     displayName = models.CharField(max_length=255)
     github = models.URLField(blank=True, null=True)
     profileImage = models.URLField(blank=True, null=True) # URL to an image
