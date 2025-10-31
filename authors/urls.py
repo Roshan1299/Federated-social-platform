f94056f60248b897609ec0f953ecac132174e5ed
Tanmay <tlad@ualberta.ca>
2025-10-31 10:33:29 -0600

    docs: small note for followers API endpoints

--- authors/urls.py
+++ authors/urls.py
@@ -57,6 +57,7 @@ urlpatterns = [
     
     # Followers API
     path("api/authors/<uuid:author_id>/followers", FollowersAPIView.as_view(), name="followers_api"),
+    # to support FQID in addition to UUID
     re_path(r'^api/authors/(?P<author_id>[0-9a-f-]+)/followers/(?P<follower_id>.+)$', 
             SingleFollowerAPIView.as_view(), name="single_follower_api"),
     
