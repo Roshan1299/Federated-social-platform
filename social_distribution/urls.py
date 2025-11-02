9909a6f0dedd7d0df4c9320abeba1b4d41b2ae97
udaymehtabsingh <158361994+udaymehtabsingh@users.noreply.github.com>
2025-11-02 06:28:51 -0700

    Image nodes implement

--- social_distribution/urls.py
+++ social_distribution/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from authors.views import CustomLoginView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
