eba690ebab5cfd323de29df94452c993b1e911d7
liamhouston <liamdh23@gmail.com>
2025-11-16 00:25:01 -0700

    fix: remove redundant image content types from Post model

--- authors/models.py
+++ authors/models.py
@@ -60,11 +60,6 @@ class Post(models.Model):
     CONTENT_TYPE_CHOICES = [
         ('text/plain', 'Plain Text'),
         ('text/markdown', 'Markdown/ CommonMark'),
-        ('image/png', 'Image/PNG'),
-        ('image/jpeg', 'Image/JPEG'),
-        ('image/gif', 'Image/GIF'),
-        ('image/bmp', 'Image/BMP'),
-        ('image/webp', 'Image/WEBP'),
     ]
     
     VISIBILITY_CHOICES = [
